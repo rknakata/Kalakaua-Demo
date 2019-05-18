c16ee265888f37ecbdb70b09bceda63b9f1fdc32
Ryan <rknakata@hawaii.edu>
2019-05-17 22:30:45 -1000

    fixed

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -14,18 +14,18 @@ For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the
 void main() // main is the function where the program will always start running from
 {
   int i; // i is a variable that can hold integers which are whole numbers
-  for(i = 1; i<=100; i++)// this is a for loop that runs the code inside of its brackets 100 times
+  for(i = 1; i<=100; i++)// this is a for loop that runs the code inside of its brackets 100 times (i is incremented everytime the loop runs until it hits 100 then the loop exits)
   {
     if(i%3 != 0 && i%5 != 0){ // if the remainder of i divided by 3 is not equal to 0 and thee remainder of i divided by 5 is not equal to 0
       printf("%d\n", i);  // prints the number
     }
-    if(i%3 == 0 && i%5 != 0){ // if the remainder of i divided by 3 is not equa$
+    if(i%3 == 0 && i%5 != 0){ // if the remainder of i divided by three is 3 and the if the remainder of i divided by 5 is not 0
       printf("Fizz\n");  // prints the number
     }
-    if(i%5 == 0 && i%3 != 0){ // if the remainder of i divided by 3 is not equa$
+    if(i%5 == 0 && i%3 != 0){ // if the remainder of i divided by 5 is equal to 0 and the remainder of i divided by 3 is not equal to 0
       printf("Buzz\n");  // prints the number
     }
-    if(i%3 == 0 && i%5 == 0){ // if the remainder of i divided by 3 is not equa$
+    if(i%3 == 0 && i%5 == 0){ // if the remainder of i divided 3 is equal to 0 and the remainder of i divided by 5 is equal to 0
       printf("FizzBuzz\n");  // prints the number
     }
   }
